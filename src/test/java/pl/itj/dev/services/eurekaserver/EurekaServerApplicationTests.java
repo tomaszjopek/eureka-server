ef38a409d09de8f1f86d84ccd18f6a2809f45369
Tomasz <tom.jopek@gmail.com>
2020-01-06 21:47:52 +0100

    'init'

---
+++ src/test/java/pl/itj/dev/services/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.itj.dev.services.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
